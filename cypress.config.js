915e0a46085f101d4080eee24b67147bf3c4f128
AndresP-XUP <andres.palvicini@encora.com>
2023-10-08 12:40:28 -0600

    adding projectId

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'a7bq2k',
   viewportHeight: 1080,
   viewportWidth: 1920,
   defaultCommandTimeout: 60000,
