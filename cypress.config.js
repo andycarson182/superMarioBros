2214301da93c58e7ba1604e100a6c8685e5f7257
AndresP-XUP <andres.palvicini@encora.com>
2023-10-08 14:25:51 -0600

    pipeFixes

--- cypress.config.js
+++ cypress.config.js
@@ -15,7 +15,6 @@ module.exports = defineConfig({
   },
   e2e: {
     setupNodeEvents(on, config) {
-      require('./cypress/plugins/index.js')(on, config)
       require('cypress-mochawesome-reporter/plugin')(on)
     },
   },
