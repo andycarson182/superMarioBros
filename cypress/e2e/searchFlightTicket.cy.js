4143131f75fba6725ac6756297dfb87166878c02
AndresP-XUP <andres.palvicini@encora.com>
2023-10-09 01:09:34 -0600

    adding small Validations

--- cypress/e2e/searchFlightTicket.cy.js
+++ cypress/e2e/searchFlightTicket.cy.js
@@ -6,6 +6,7 @@ import searchPage from '../support/page-objects/SearchPage';
 describe('Test the Avisales ticket search functionality', () => {
   beforeEach(() => {
     cy.visit('https://www.aviasales.com/');
+    cy.title().should('eq', 'Cheap Flights, Airline Tickets & Airfares - Find Deals on Flights at Aviasales.com');
   })
 
   it('Verify the user is able to do a ticket search.', () => {
@@ -20,7 +21,7 @@ describe('Test the Avisales ticket search functionality', () => {
       homePage.selectPassengerTypeAndAddPassengers(flightInfo.passengerType, flightInfo.passengerClass, flightInfo.numberOfPassengers);
       commonPageElements.clickSearchFlightsButton();
       searchPage.checkNewSearchPageisOpen();
-      commonPageElements.checkNightBackgroundIsEnabled(true);
+      commonPageElements.checkNightBackgroundIsEnabled(true); // checks the night background is still enabled in the search page
       searchPage.checkValuesOnFilterForm(
         flightInfo.expectedOrigin,
         flightInfo.destination,
